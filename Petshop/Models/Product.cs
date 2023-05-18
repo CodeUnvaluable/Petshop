73a6220d4862697b1c9d2942cc8545d055ea2909
Dev_Tae <66623@LAPTOP-AACHH0UO>
2023-05-18 14:32:44 +0700

    Add layout

---
+++ Petshop/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Petshop.Context;
+using AutoMapper;
+using Microsoft.Ajax.Utilities;
+
+namespace Petshop.Models
+{
+    public class Product
+    {
+        MapperConfiguration config = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<ProductModel, Context.Product>();
+            cfg.CreateMap<Context.Product, ProductModel>();
+        });
+    }
+    public class ProductModel : Context.Product
+    {
+
+    }
+}
